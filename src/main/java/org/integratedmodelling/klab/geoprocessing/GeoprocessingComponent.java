cf1f3c2892dbd33ba1feb424a3a828ccf1c9c20a
Ferdinando Villa <ferdinando.villa@bc3research.org>
2023-08-15 10:27:52 +0200

    Add stub plugin configuration

--- src/main/java/org/integratedmodelling/klab/geoprocessing/GeoprocessingComponent.java
+++ src/main/java/org/integratedmodelling/klab/geoprocessing/GeoprocessingComponent.java
@@ -1,5 +1,31 @@
 package org.integratedmodelling.klab.geoprocessing;
 
-public class GeoprocessingComponent {
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+
+public class GeoprocessingComponent extends Plugin {
+
+    public GeoprocessingComponent(PluginWrapper wrapper) {
+        super(wrapper);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        super.stop();
+    }
+
+    @Override
+    public void delete() {
+        // TODO Auto-generated method stub
+        super.delete();
+    }
 
 }
